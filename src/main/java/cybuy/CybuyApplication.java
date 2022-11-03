6b82a6fad3e3bd1f5847079d0eb8d889ffba03bd
Justus <justus.necker@gmail.com>
2022-11-03 14:22:54 +0100

    Project scaffolding

---
+++ src/main/java/cybuy/CybuyApplication.java
@@ -0,0 +1,13 @@
+package cybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybuy/CybuyApplicationTests.java
@@ -0,0 +1,13 @@
+package cybuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
