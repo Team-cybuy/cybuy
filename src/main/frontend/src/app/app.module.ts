623c82816787392903fe9ad3479522a943bc1ca7
entrisity <entrisity@gmail.com>
2022-11-15 18:22:48 +0100

    Placeholder for the homepage
    - added component for navbar
    - added image folder inside of the "src/assets" folder
    - added logo svg

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
