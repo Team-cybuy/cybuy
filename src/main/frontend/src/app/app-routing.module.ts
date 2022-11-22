c2cf04bbcd5716b15856d72f93c8ee823d1aa736
Dynamotivation <32247653+Dynamotivation@users.noreply.github.com>
2022-11-22 16:05:19 +0100

    Integrated Listing with Backend

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'item', component: ListingComponent },
+  { path: 'item/:itemId', component: ListingComponent },
 ];
 
 @NgModule({
